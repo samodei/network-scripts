87cecdeb125fbe1ea30af58c9627a09d233d1be2
Stefano Amodei <stefano.amodei@pm.me>
2021-11-15 11:56:16 -0500

    Added backup config.

--- hp/procurve.py
+++ hp/procurve.py
@@ -1,14 +1,21 @@
 #!/bin/python3
 
 import argparse
+import datetime
 import getpass
+import sys
 
 from netmiko import ConnectHandler
 
 def send(net_connect, command):
-    print(net_connect.send_command(command))
+    output = (net_connect.send_command(command))
+    print(output)
+
+    # Need to return for functions that need output set to a string.
+    return output
 
 def get_mac(mac):
+    # Print MAC address table or search a specific MAC or port if provided.
     if mac == "not_specified":
         command = "show mac-address"
     elif mac != "not_specified":
@@ -20,25 +27,30 @@ def get_mac(mac):
 
 
 def get_vlan(vlan):
+    # Print VLANs.
     command = "show vlans"
     return command
 
 
 def get_config_status(config):
+    # Print running config status.
     command = "show config status"
     return command
 
 
-def backup_config():
-    # TODO
-    backup = "show config"
+def backup_config(backup):
+    # I needed to change this. This will not return a command like the others.
+    current_time = datetime.datetime.today().strftime('%Y_%m_%d')
+    with open('hp_switch_backup_' + str(current_time) + '.cfg', 'w') as f:
+        for line in backup:
+            f.write(line)
 
 
 def main():
     # Create the parser and arguments.
     parser = argparse.ArgumentParser()
     parser.add_argument("host", help="IP Address of the device.")
-    parser.add_argument("-b", "--backup-config", action="store_true", 
+    parser.add_argument("-b", "--backup", action="store_true", 
             help="Backup the startup config.")
     parser.add_argument("-c", "--config", action="store_true", help="Show config status.")
     parser.add_argument("-m", "--mac", nargs='?', const='not_specified', 
@@ -65,13 +77,20 @@ def main():
         command = get_vlan(args.vlan)
     if args.config:
         command = get_config_status(args.config)
+    if args.backup:
+        command = None
+        backup = send(net_connect, "show config")
+        backup_config(backup)
 
     # Send command.
-    if command:
+    if command is not None:
         send(net_connect, command)
+    elif command is None:
+        sys.exit()
     
     # Disconnect the SSH connection.
     net_connect.disconnect()
 
 if __name__ == '__main__':
     main()
+    sys.exit()
